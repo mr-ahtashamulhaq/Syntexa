01ec064a1f73e2fbf2f3f64cd1af839c0a769d5b
Mr. Ahtasham Ul Haq <mr.ahtashamulhaq@gmail.com>
2026-05-28 18:44:28 +0500

    Formatted correct and remove docx loading feature

--- Document.h
+++ Document.h
@@ -4,103 +4,107 @@
 #include <string>
 #include <algorithm>
 #include <cstdlib>
-//#include"ConsoleHelper.h"
 
 using namespace std;
 
-class TextReader {
+class TextReader 
+{
+    public:
+        string loadContent(string filename)
+        {
 
-public:
+            string ext = getExtension(filename);
 
-    string loadContent(string filename) {
+            if (ext == "txt")
+            {
+            return readTXT(filename);
+            }
 
-        string ext = getExtension(filename);
+            else if (ext == "pdf")
+            {
+                return readPDF(filename);
+            }
 
-        if (ext == "txt"){
-           return readTXT(filename);
+            else 
+            {
+                cout << "Unsupported file format: " << ext << endl;
+                return "";
+            }
         }
 
-        else if (ext == "pdf"){
-            return readPDF(filename);
+    private:
+
+        // grab extension
+        string getExtension(const string& filename)
+        {
+            size_t dot = filename.find_last_of('.'); //Find last occurance of  '.' in the file name
+            if (dot == string::npos) return "";
+
+            string exten = filename.substr(dot + 1); //grab everything after dot
+
+            transform(exten.begin(), exten.end(), exten.begin(), :: tolower);  //convert to lower case
+            
+            return exten;
         }
 
-        else if (ext == "docx"){
-            return readDOCX(filename);
-        }
-        else {
-            cout << "Unsupported file format: " << ext << endl;
-            return "";
-        }
-    }
+        // Text File Record
+        string readTXT(const string& filename)
+        {
 
-private:
+            ifstream infile(filename);
 
-    // grab extension
-    string getExtension(const string& filename) {
-        size_t dot = filename.find_last_of('.');
-        if (dot == string::npos) return "";
+            if (!infile.is_open())
+            {
+                cout << "File not found: " << filename << endl;
+                return "";
+            }
 
-        string ext = filename.substr(dot + 1);
-        transform(ext.begin(), ext.end(), ext.begin(), ::tolower);
-        return ext;
-    }
+            string line, content;
 
-    // Text File Record
-    string readTXT(const string& filename) {
+            while (getline(infile, line))
+            {
+                content += line + " ";
+            }
 
-        ifstream infile(filename);
-
-        if (!infile.is_open()) {
-            cout << "File not found: " << filename << endl;
-            return "";
+            return content;
         }
 
-        string line, content;
+        // PDF Read 
+        string readPDF(const string& filename)
+        {
 
-        while (getline(infile, line)) {
-            content += line + " ";
+            string exePath  = "poppler-26.02.0\\Library\\bin\\pdftotext.exe";
+            string input    = filename;
+            string output   = "temp.txt";
+
+            // This is Basically a command to run in terminal in the background,
+            // It takes the PDF file and converts it to a text file.
+            string command = "cmd /C \"\"" + exePath + "\" -enc UTF-8 \"" + input + "\" \"" + output + "\" > nul 2>&1\"";
+
+            int result = system(command.c_str()); // This line will execute the above command. return 0 if it extract successfully
+
+            if (result != 0)
+            {
+                cout << "External utility execution failed!"<<endl;
+                return "";
+            }
+
+            ifstream file(output); //Opens the file which poppler just generated
+            if (!file.is_open())
+            {
+                cout << "PDF conversion failed\n";
+                return "";
+            }
+
+            string content;
+            string line;
+            while(getline(file,line))
+            {
+                content += line + " ";
+            }
+            
+            file.close(); 
+
+            return content;
         }
-
-        return content;
-    }
-
-    // PDF Read 
-    string readPDF(const string& filename) {
-
-    string basePath = "C:\\Users\\hasnain ali\\Desktop\\DSA Clone Project\\Syntexa\\";
-    string exePath  = basePath + "poppler-26.02.0\\Library\\bin\\pdftotext.exe";
-    string input    = filename;
-    string output   = basePath + "temp.txt";
-
-    string command =
-        "cmd /C \"\"" + exePath + "\" -enc UTF-8 \"" + input + "\" \"" + output + "\" > nul 2>&1\"";
-
-        int result = system(command.c_str());
-
-        if (result != 0) {
-        cout << "External utility execution failed\n";
-        return "";
-    }
-
-        ifstream file(output);
-        if (!file.is_open()) {
-            cout << "PDF conversion failed\n";
-            return "";
-        }
-
-        string content;
-        string line;
-        while(getline(file,line)){
-            content += line + " ";
-        }
-        
-        file.close(); 
-
-        return content;
-    }
-
-    string readDOCX(const string& filename) {
-        cout << "DOCX support not implemented yet (requires XML parsing)\n";
-        return "";
-    }
 };
